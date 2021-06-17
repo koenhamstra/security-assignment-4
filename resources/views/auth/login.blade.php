e5a98a989ff80b5203b8aceaaab07bbf9a5faa84
Koen Hamstra <koen@Koens-MacBook-Pro.local>
2021-06-17 10:13:38 +0200

    working OAuth

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -161,7 +161,12 @@
                     <x-button class="ml-3">
                         {{ __('Log in') }}
                     </x-button>
-                        <a href="login/github" class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 active:bg-gray-900 focus:outline-none focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25 transition ease-in-out duration-150 ml-3">Login with Github</a>
+                    <a href="login/github" class="inline-flex items-center
+                        px-4 py-2 bg-gray-800 border border-transparent rounded-md
+                        font-semibold text-xs text-white uppercase tracking-widest
+                        hover:bg-gray-700 active:bg-gray-900 focus:outline-none
+                        focus:border-gray-900 focus:ring ring-gray-300 disabled:opacity-25
+                        transition ease-in-out duration-150 ml-3">Login with Github</a>
                 </div>
             </form>
         </x-auth-card>
